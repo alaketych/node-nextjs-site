54be5a4635778eb0ebfd0df3b891bad70d679c42
alaketych <alaketych@ukr.net>
2020-11-16 21:07:16 +0200

    data models

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,8 +1,19 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/', (request, response) => {
-    response.send('asd')
-})
+//const records = require('../data/_index')
+
+const ad = require('../data/recordPost')
+
+const postsConteroller = require('../controllers/postControllers')
+const projectController = require('../controllers/projectController')
+
+router.get('/posts', postsConteroller.getPosts)
+router.get('/projects', projectController.getProjects)
+
+router.get('/post/:id', postsConteroller.getPost)
+router.get('/project/:id', projectController.getProject)
+
+router.post('/contact')
 
 module.exports = router
