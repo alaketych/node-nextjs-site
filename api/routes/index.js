d01ec4dcaaeaa03c381eb5dc7da8da3ea98d92de
alaketych <alaketych@ukr.net>
2020-11-17 21:46:57 +0200

    http api

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,17 +1,18 @@
 const express = require('express')
 const router = express.Router()
 
-//const records = require('../data/_index')
+//const datarecords = require('../data/_index')
+const database = require('../controllers/database')
+const postsController = require('../controllers/Post')
+const projectController = require('../controllers/Project')
 
-const ad = require('../data/recordPost')
 
-const postsConteroller = require('../controllers/postControllers')
-const projectController = require('../controllers/projectController')
+database.sync({ force: false })
 
-router.get('/posts', postsConteroller.getPosts)
+router.get('/posts', postsController.getPosts)
 router.get('/projects', projectController.getProjects)
 
-router.get('/post/:id', postsConteroller.getPost)
+router.get('/post/:id', postsController.getPost)
 router.get('/project/:id', projectController.getProject)
 
 router.post('/contact')
