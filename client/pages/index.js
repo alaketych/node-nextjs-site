081914ed60d30804602bd5ec4eca64bb32c46255
alaketych <alaketych@ukr.net>
2020-11-18 16:02:05 +0200

    home page

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,65 +1,141 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import React from 'react'
+import { getPosts, getProjects } from './api/fetch'
+import dynamic from 'next/dynamic'
+import { Banner, Button, Quote, Article, Service, ProjectView } from '../components/_index'
 
-export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+dynamic(
+    () => import('@lottiefiles/lottie-player'), { ssr: false }
+)
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+function Home({ projects, articles }) {
+    return (
+        <div className="App">
+            <Banner
+                title="Hi, I'm Alexandr"
+                description="A web developer from Kyiv, Ukraine. I create bespoke websites to help people go further online.">
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+                <lottie-player
+                    src="https://assets1.lottiefiles.com/private_files/lf30_WdTEui.json"
+                    loop
+                    autoplay
+                    speed="0.75"
+                    renderer="svg"
+                />
+            </Banner>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+            <section className="block huge-spacing">
+                <div className="wrapper">
+                    <h3 className="title-section">FEATURED PROJECT</h3>
+                    <div className="divider"></div>
+                    {
+                        projects.map(project => {
+                            return(
+                                <ProjectView
+                                    key={ project.id }
+                                    title={ project.title }
+                                    photo={ project.photo }
+                                    preview={ project.preview }
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+                                    className="content"
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
+                                    link={ project.id }
+                                    github_link={ project.githublink }
+                                    //className="content-reversed"
+                                />
+                            )
+                        })
+                    }
+                </div>
+            </section>
 
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+
+            <Quote
+                client="Sergej Mason"
+                company="Tix24"
+                companyURL="https://tix24.com.ua/"
+                feedback="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit debitis explicabo obcaecati itaque consectetur, quod eaque minima distinctio culpa numquam ex amet, provident consequatur, dolor reprehenderit vitae mollitia nulla vero!
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit debitis explicabo obcaecati itaque consectetur, quod eaque minima distinctio culpa numquam ex amet, provident consequatur, dolor reprehenderit vitae mollitia nulla vero!"
+            />
+
+            <section className="block spacing-tiny-top">
+                <div className="wrapper">
+                    <h3 className="title-section">My services</h3>
+                    <div className="divider"></div>
+
+                    <div className="array huge-spacing">
+                        <Service
+                            title="Web design"
+                            description="A website should be designed for the people who will use it, so that's exactly what I do. User focused design should be the primary goal of any website."
+                            svgPath="M7 17H2a2 2 0 0 1-2-2V2C0 .9.9 0 2 0h16a2 2 0 0 1 2 2v13a2 2 0 0 1-2 2h-5l4 2v1H3v-1l4-2zM2 2v11h16V2H2z"
+                        />
+
+                        <Service
+                            title="Web development"
+                            description="A website should be designed for the people who will use it, so that's exactly what I do. User focused design should be the primary goal of any website."
+                            svgPath="M11 9.27V0l6 11-4 6H7l-4-6L9 0v9.27a2 2 0 1 0 2 0zM6 18h8v2H6v-2z"
+                        />
+                    </div>
+
+                    <Button
+                        className="foreground center"
+                        label="Discover more"
+                        link="/about"
+                    />
+                </div>
+            </section>
+
+            <Quote
+                client="Sergej Mason"
+                company="Tix24"
+                companyURL="https://tix24.com.ua/"
+                feedback="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit debitis explicabo obcaecati itaque consectetur, quod eaque minima distinctio culpa numquam ex amet, provident consequatur, dolor reprehenderit vitae mollitia nulla vero!
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit debitis explicabo obcaecati itaque consectetur, quod eaque minima distinctio culpa numquam ex amet, provident consequatur, dolor reprehenderit vitae mollitia nulla vero!"
+            />
+
+            <section className="block spacing-tiny-top">
+                <div className="wrapper">
+                    <h3 className="title-section">From my blog</h3>
+                    <div className="divider"></div>
+
+                    <div className="array huge-spacing">
+                        {
+                            articles.map(article => {
+                                return (
+                                    <Article
+                                        key={ article.id }
+                                        link={ article.id }
+                                        article={ article.title }
+                                        category={ null }
+                                    />
+                                )
+                            })
+                        }
+                    </div>
+
+                    <Button
+                        className="foreground center"
+                        label="View more posts"
+                        link="/posts"
+                    />
+                </div>
+            </section>
         </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+    )
 }
+
+export async function getStaticProps() {
+    const responsePosts = await getPosts()
+    const responseProjects = await getProjects()
+
+    const articles = await responsePosts.json()
+    const projects = await responseProjects.json()
+
+    return {
+        props: {
+            articles,
+            projects
+        }
+    }
+}
+
+export default Home
