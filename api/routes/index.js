52805e9c6cdaa7ff94cd37e12d968604d97caf0f
alaketych <alaketych@ukr.net>
2020-11-28 21:15:09 +0200

    mail sender

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const router = express.Router()
+const nodemailer = require('nodemailer')
+const multiparty = require('multiparty')
 
 const database = require('../controllers/database')
 const postsController = require('../controllers/Post')
 const projectController = require('../controllers/Project')
+const { request, response } = require('express')
 
 database.sync({ force: true })
 
@@ -12,13 +15,39 @@ router.get('/projects',            projectController.getProjects)
 router.get('/post/:id',            postsController.getPost)
 router.get('/project/:id',         projectController.getProject)
 
-router.get('/contact', (request, response) => {
-    response.send('asd')
-})
-
 router.post('/contact', (request, response) => {
     console.log('Got body: ', request.body)
     response.send('arrived.')
+})
+
+router.get('/contact', (request, response) => {
+    let transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 587,
+        secure: false,
+        auth: {
+            user: "xxx",
+            pass: "xxx",
+        }
+    })
+
+    let info = transporter.sendMail({
+        from: "xxx",
+        to: "xxx",
+        subject: "Hello, this is from vscode.",
+        html: "<b>Hello, this is from vscode.</b>",
+    })
+
+    // let form = new multiparty.Form()
+    // let data = {};
+    // form.parse(request, (error, fields) => {
+    //     Object.keys(fields).forEach(property => {
+    //         data[property] = fields[property].toString()
+    //     })
+
+    //     const mail = {
+    //     }
+    // })
 })
 
 module.exports = router
